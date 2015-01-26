73b16ba725420bbf4db885046d107f8731b9c8be
asuke <asuke@www3227gj.sakura.ne.jp>
2015-01-26 15:18:55 +0900

    Add databases-connect-code.

--- main.rb
+++ main.rb
@@ -1,12 +1,25 @@
 require 'sinatra'
+require 'active_record'
+require 'yaml'
 require "json"
 require "json-fuzz-generator"
 require 'logger'
 
-logger = Logger.new(STDOUT)
-
+# start setting for server run 
 set :environment, :production
 
+# connecting databases
+config = YAML.load_file('database.yml')
+ActiveRecord::Base.establish_connection(config["prod"])
+
+# log setting
+logger = Logger.new(STDOUT)
+
+#db override
+class Apps < ActiveRecord::Base
+end
+
+# main codes
 get '/' do
   erb :index
 end
@@ -14,13 +27,7 @@ end
 get'/generate_data.json' do
     content_type :json
     if @params[:id] == "12" then
-        return [ 
-          {:id => 1234, :name => "tanaka"},
-          {:id => 2344, :name => "misono6"},
-          {:id => 2345, :name => "misono4"},
-          {:id => 2346, :name => "misono3"},
-          {:id => 2343, :name => "misono"}
-        ].to_json
+        return Apps.all.to_json
     end
     [ 
       {:id => 1234, :name => "tanaka"},
